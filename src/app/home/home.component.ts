ce6d33b2ab1896e107b5147b7b331029620b3b90
natsukidxd <natsukid123@gmail.com>
2026-05-10 11:23:13 +0800

    feat: create home component with user greeting and account information display

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { AccountService } from '@app/_services';
+import { Account } from '@app/_models';
+
+@Component({
+  templateUrl: './home.component.html',
+  standalone: false
+})
+export class HomeComponent {
+  account: Account | null;
+
+  constructor(private accountService: AccountService) {
+    this.account = this.accountService.accountValue;
+  }
+}
